a0afe06f7959dd307d30968c4d839a79ed8e34e7
cdg77 <cdegregorio@gmail.com>
2015-09-25 15:53:04 -0700

    return records stored in a database

--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
     modulePrefix: 'knowledgement',
     environment: environment,
     contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
-    firebase: 'https://YOUR-FIREBASE-NAME.firebaseio.com/',
+    firebase: 'hhttps://knowledgement.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
