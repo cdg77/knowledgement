0e083418801638f2902d64c897e795818fb601a9
cdg77 <cdegregorio@gmail.com>
2015-09-29 22:13:56 -0700

    NOW it adds new questions to the datatbase

--- app/components/update-question.js
+++ app/components/update-question.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  updateQuestionForm: false,
+  actions: {
+    updateQuestionFormShow() {
+      this.set('updateQuestionForm', true);
+    },
+    update(question) {
+      var params= {
+        author: this.get('author'),
+        image: this.get('image'),
+        question: this.get('question'),
+        note: this.get('note')
+      };
+      this.set('updateQuestionForm', false),
+      this.sendAction('update', question, params);
+    }
+  }
 });
