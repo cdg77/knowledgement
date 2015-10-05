a7dc7d1c7eb5829b3bdb75686e9ff6706817117b
cdg77 <cdegregorio@gmail.com>
2015-10-04 17:19:17 -0700

    adding ability to answers questions

--- app/components/question-detail.js
+++ app/components/question-detail.js
@@ -1,4 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  updateQuestion: false,
+  actions: {
+    updateQuestionForm() {
+      this.set('updateQuestionForm', true);
+    },
+
+    updateQuestion(question) {
+      var params = {
+        author: this.get('author'),
+        category: this.get('category'),
+        date: this.get('date'),
+        image: this.get('image'),
+        location: this.get('location'),
+        title: this.get('title')
+      };
+      this.set('updateQuestionForm', false),
+      this.sendAction('updateQuestion', question, params);
+    }
+  }
 });
--- app/models/question.js
+++ app/models/question.js
@@ -4,5 +4,6 @@ export default DS.Model.extend({
   author: DS.attr(),
   question: DS.attr(),
   note: DS.attr(),
-  image: DS.attr()
+  image: DS.attr(),
+  answers: DS.hasMany('answer', {async: true})
 });
